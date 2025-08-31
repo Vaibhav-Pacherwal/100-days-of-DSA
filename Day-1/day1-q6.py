de539e435b809de36b13c78af721f3aadbd19d51
Vaibhav-Pacherwal <vaibhavpacherwal57@gmail.com>
2025-08-31 22:31:34 +0000

    Day-1

--- Day-1/day1-q6.py
+++ Day-1/day1-q6.py
@@ -1,9 +1,12 @@
-## Find the union
-nums1 = [1, 2, 3, 4, 5]
-nums2 = [3, 4, 5, 6, 7]
+## Find the missing number in an array
+nums = [1,2,4,5,0]
 
-nums_set1 = set(nums1)
-nums_set2 = set(nums2)
-new_set = nums_set1.union(nums_set2)
-new_nums_lst = list(new_set)
-print(new_nums_lst)
+def missingVal(nums):
+    for i in range(0, len(nums)):
+        if nums[i] != i+1:
+            return i+1
+        
+    return -1
+
+missingIdx = missingVal(nums)
+print(f"{missingIdx} is missing!")
